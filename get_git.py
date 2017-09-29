aba8fe2d467af4eba28c65e40de65e7b29fcf404
Lee Jacobs <pieistehgood@gmail.com>
2017-09-28 23:22:46 -0700

    try some shit

--- get_git.py
+++ get_git.py
@@ -5,13 +5,14 @@ GPIO.setmode(GPIO.BCM)
 
 input_pin = 23
 
-GPIO.setup(input_pin, GPIO.IN)
+GPIO.setup(input_pin, GPIO.IN, pull_up_down==GPIO.PUD_UP)
 
 try:
 	while True:
-		reading = GPIO.input(input_pin)
-		print(reading)
-		time.sleep(0.5)
+		if GPIO.input(input_pin) == False:
+			print("Button Pressed!")
+			while GPIO.input(input_pin) == False:
+				time.sleep(0.1)
 finally:
 	print("cleaning up")
 	GPIO.cleanup()
