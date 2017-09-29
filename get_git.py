b4bb7cfb1f1090bc76e7ecca5ae9ef2165734803
Lee Jacobs <pieistehgood@gmail.com>
2017-09-28 23:42:48 -0700

    test commit

--- get_git.py
+++ get_git.py
@@ -11,7 +11,7 @@ GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 try:
 	while True:
 		if GPIO.input(input_pin) == False:
-			print("Pulling most recent code!")
+			print("Pulling most recent code!!")
 			subprocess.call(['./pull_projects.sh'])
 			while GPIO.input(input_pin) == False:
 				time.sleep(0.1)
