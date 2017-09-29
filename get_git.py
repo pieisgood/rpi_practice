dc3dcedea6510255bf79a71e077e4ebb800519e2
Lee Jacobs <pieistehgood@gmail.com>
2017-09-28 22:31:32 -0700

    Listen for GPIO pin

---
+++ get_git.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+input_pin = 23
+
+GPIO.setup(input_pin, GPIO.IN)
+
+try:
+	while True:
+		reading = GPIO.input(input_pin)
+		print(reading)
+		time.sleep(0.5)
+finally:
+	print("cleaning up")
+	GPIO.cleanup()
